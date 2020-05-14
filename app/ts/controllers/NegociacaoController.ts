a6d4c95ce7b647739f4f203530ec5e0c1448c0cf
Nayra Oliveira <nayrabiaoliveira@gmail.com>
2020-05-14 08:57:31 -0300

    Organização de módulo em barris

--- app/ts/controllers/NegociacaoController.ts
+++ app/ts/controllers/NegociacaoController.ts
@@ -1,7 +1,5 @@
-import { Negociacao } from '../models/Negociacao';
-import { Negociacoes } from '../models/Negociacoes';
-import { NegociacoesView } from '../views/NegociacoesView';
-import { MensagemView } from '../views/MensagemView';
+import { Negociacao, Negociacoes } from '../models/index';
+import { MensagemView, NegociacoesView } from '../views/index';
 
 export class NegociacaoController{
 
